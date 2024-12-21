86a10ab30f9aff6c698122796a6bd0cd6237492f
tegar-ganang <tegarsatrio10@gmail.com>
2024-12-22 02:20:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import folium
-import matplotlib
+import matplotlib as plt
 from streamlit_folium import folium_static
 from dataprep.eda import create_report
 from streamlit.components.v1 import html
